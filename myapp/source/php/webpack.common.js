90531eeac5e89b55c0c9a2979d4f6fa84cfe9089
ison <hidekiisobe9@gmail.com>
2020-03-06 18:53:31 +0900

    Webpack設定ファイルを開発用と本番用に分割

---
+++ myapp/source/php/webpack.common.js
@@ -0,0 +1,67 @@
+// webpack.config.js
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const prefixUri = "/myapp";
+
+module.exports = {
+    resolve: {
+        modules: [
+            path.resolve('./src'),
+            path.resolve('./node_modules')
+        ]
+    },
+    entry: path.resolve(__dirname, 'src', 'main.js'),
+    output: {
+        path: path.resolve(__dirname, 'public', 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                loader: "file-loader",
+                options: {
+                    name: '[path][name].[ext]',
+                    outputPath: 'images/',
+                    publicPath: function (filePath) {
+                        return prefixUri + '/dist/images/' + filePath;
+                    }
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+                loader: "file-loader",
+                options: {
+                    name: '[path][name].[ext]',
+                    outputPath: 'fonts/',
+                    publicPath: function (filePath) {
+                        return prefixUri + '/dist/fonts/' + filePath;
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
