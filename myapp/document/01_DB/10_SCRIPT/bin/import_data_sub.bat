a3a5a9ed04ea06a88d470527c432e3cc24ffdc19
ison <hidekiisobe9@gmail.com>
2020-03-19 14:53:16 +0900

    外部キー制約によりtruncateに失敗するためtruncate時に一時的に外部キー制約をオフにするように修正

--- myapp/document/01_DB/10_SCRIPT/bin/import_data_sub.bat
+++ myapp/document/01_DB/10_SCRIPT/bin/import_data_sub.bat
@@ -5,7 +5,7 @@ call %curpath%\basename.bat "%1"
 
 echo %FILE_NAME_WITHOUT_EXT%:ÉfÅ[É^ìäì¸
 
-%mysql% %option% -h %host% %database% -u %user% -p%password% -e"truncate %FILE_NAME_WITHOUT_EXT%;"
+%mysql% %option% -h %host% %database% -u %user% -p%password% -e"set foreign_key_checks = 0; truncate %FILE_NAME_WITHOUT_EXT%; set foreign_key_checks = 1;"
 IF NOT %ERRORLEVEL% == 0 (
 	exit /b %ERRORLEVEL%
 )
