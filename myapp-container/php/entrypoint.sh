5476be0e77e07a8436ced4d12acb6b8e215f0480
Hideki Isobe <hideki.isobe@ison-t.com>
2022-02-27 21:39:29 +0900

    Docker Container変更

--- myapp-container/php/entrypoint.sh
+++ myapp-container/php/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-composer install --working-dir="/opt/myapp"
+if [ "$ENVIRONMENT" != "development" ] ; then
+    composer install --working-dir="/opt/myapp"
+    npm ci --prefix "/opt/myapp"
+    npm run build --prefix "/opt/myapp"
+fi
+
 chown -R www-data:www-data "/opt/myapp"
 chmod -R 774 "/opt/myapp"
-
-npm ci --prefix "/opt/myapp"
-npm run build --prefix "/opt/myapp"
 
 php-fpm
